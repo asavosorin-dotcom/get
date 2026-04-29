11a8c1e89221941996bca5f9d1c165aa0cfaba19
asavosorin-dotcom <asavosorin@gmail.com>
2026-04-29 19:29:04 +0300

    c

--- get-adc/r2r-sc.py
+++ get-adc/r2r-sc.py
@@ -12,14 +12,14 @@ duration = 3.0
 try:
     start_time = time.time()
 
-    while (time.time() - start_time) < DURATION:
+    while (time.time() - start_time) < 3:
         voltage = adc.get_sc_voltage()
         current_time = time.time() - start_time
 
         voltage_values.append(voltage)
         time_values.append(current_time)
 
-    plot_voltage_vs_time(time_values, voltage_values, DYNAMIC_RANGE)
+    plot_voltage_vs_time(time_values, voltage_values, 3.3)
     plot_sampling_period_hist(time_values)
 
 finally:
